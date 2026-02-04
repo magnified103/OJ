0a9818ecc7e55f59b5d8423b5d35cb0ff8016969
whlongg <nhl08.contact@gmail.com>
2026-02-04 10:57:54 +0700

    Apply suggestions from code review
    Co-authored-by: Dung Nguyen <dung@ekluster.com>

--- .ci.settings.py
+++ .ci.settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'NAME': 'dmoj',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': os.environ.get('DB_HOST', '127.0.0.1'),
-        'PORT': os.environ.get('DB_PORT', '3307'),
+        'HOST': 'mysql',
+        'PORT': 3306,
         'OPTIONS': {
             'charset': 'utf8mb4',
         },
