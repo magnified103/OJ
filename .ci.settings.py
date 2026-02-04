056b08dd62c67f5eb63a6b2abce48f8fbe2cba58
Nguyen Viet Dung <29406816+magnified103@users.noreply.github.com>
2026-02-04 15:50:40 +0700

    test

--- .ci.settings.py
+++ .ci.settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'NAME': 'dmoj',
         'USER': 'root',
         'PASSWORD': 'root',
-        'HOST': 'mysql',
+        'HOST': '127.0.0.1',
         'PORT': 3306,
         'OPTIONS': {
             'charset': 'utf8mb4',
